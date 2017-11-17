dbf0d9e477a3e2782abda0ae7e59a69abf7c54b9
QuickShadow <32963102+QuickShadow@users.noreply.github.com>
2017-11-17 18:16:21 +0000

    Added dictionary translation
    Compares input to dictionary definitions.
    Added several rooms to ground floor.

--- TextAdventureMaster.py
+++ TextAdventureMaster.py
@@ -11,6 +11,8 @@ actionDictionary ["q"] = "quit"
 actionDictionary ["end"] = "quit"
 actionDictionary ["look"] = "look"
 actionDictionary ["l"] = "look"
+actionDictionary ["save"] = "save"
+actionDictionary ["load"] = "load"
 
 directionDictionary = {}
 directionDictionary ["north"] = "north"
@@ -35,7 +37,8 @@ itemDictionary ["ladder"]= "ladder"
 itemDictionary ["lamp"] = "lamp"
 itemDictionary ["key"] = "key"
 itemDictionary ["telescope"] = "telescope"
-
+itemDictionary ["matches"] = "matches"
+itemDictionary ["matchbox"] = "matches"
 
 
 roomStatus      = {}
@@ -47,6 +50,10 @@ roomWest        = {}
 roomUp          = {}
 roomDown        = {}
 roomItems       = {}
+
+## Basement
+
+## Ground Floor
 
 roomStatus      ["FrontDoor"] = 0
 roomDescription ["FrontDoor"] = ["This a a description of the FrontDoor."]
@@ -70,13 +77,75 @@ roomItems       ["EntryHall"] = ["key", "candle", "candle"]
 
 roomStatus      ["MainHall"] = 0
 roomDescription ["MainHall"] = ["This a a description of the Main Hall."]
-roomNorth       ["MainHall"] = "null"
+roomNorth       ["MainHall"] = "DiningHall"
 roomEast        ["MainHall"] = "null"
 roomSouth       ["MainHall"] = "EntryHall"
-roomWest        ["MainHall"] = "null"
-roomUp          ["MainHall"] = "null"
+roomWest        ["MainHall"] = "Library"
+roomUp          ["MainHall"] = "CentralLanding"
 roomDown        ["MainHall"] = "null"
 roomItems       ["MainHall"] = []
+
+roomStatus      ["DiningHall"] = 0
+roomDescription ["DiningHall"] = ["This a a description of the Dining Hall."]
+roomNorth       ["DiningHall"] = "Solarium"
+roomEast        ["DiningHall"] = "ServiceArea"
+roomSouth       ["DiningHall"] = "MainHall"
+roomWest        ["DiningHall"] = "null"
+roomUp          ["DiningHall"] = "null"
+roomDown        ["DiningHall"] = "null"
+roomItems       ["DiningHall"] = []
+
+roomStatus      ["ServiceArea"] = 0
+roomDescription ["ServiceArea"] = ["This a a description of the Service Area."]
+roomNorth       ["ServiceArea"] = "null"
+roomEast        ["ServiceArea"] = "ServantsHall"
+roomSouth       ["ServiceArea"] = "null"
+roomWest        ["ServiceArea"] = "DiningHall"
+roomUp          ["ServiceArea"] = "null"
+roomDown        ["ServiceArea"] = "ServantsStairs"
+roomItems       ["ServiceArea"] = []
+
+roomStatus      ["Library"] = 0
+roomDescription ["Library"] = ["This a a description of the Library."]
+roomNorth       ["Library"] = "HiddenRoom"
+roomEast        ["Library"] = "MainHall"
+roomSouth       ["Library"] = "Study"
+roomWest        ["Library"] = "null"
+roomUp          ["Library"] = "null"
+roomDown        ["Library"] = "null"
+roomItems       ["Library"] = []
+
+roomStatus      ["Study"] = 0
+roomDescription ["Study"] = ["This a a description of the Study."]
+roomNorth       ["Study"] = "Library"
+roomEast        ["Study"] = "null"
+roomSouth       ["Study"] = "EntryHall"
+roomWest        ["Study"] = "Library"
+roomUp          ["Study"] = "null"
+roomDown        ["Study"] = "null"
+roomItems       ["Study"] = []
+
+roomStatus      ["HiddenRoom"] = 0
+roomDescription ["HiddenRoom"] = ["This a a description of the Hidden Room."]
+roomNorth       ["HiddenRoom"] = "null"
+roomEast        ["HiddenRoom"] = "null"
+roomSouth       ["HiddenRoom"] = "null"
+roomWest        ["HiddenRoom"] = "null"
+roomUp          ["HiddenRoom"] = "null"
+roomDown        ["HiddenRoom"] = "null"
+roomItems       ["HiddenRoom"] = []
+
+## First Floor
+
+roomStatus      ["CentralLanding"] = 0
+roomDescription ["CentralLanding"] = ["This a a description of the Central Landing."]
+roomNorth       ["CentralLanding"] = "null"
+roomEast        ["CentralLanding"] = "null"
+roomSouth       ["CentralLanding"] = "null"
+roomWest        ["CentralLanding"] = "null"
+roomUp          ["CentralLanding"] = "null"
+roomDown        ["CentralLanding"] = "MainHall"
+roomItems       ["CentralLanding"] = []
 
 itemList = []
 currentRoom = "FrontDoor"
@@ -87,23 +156,23 @@ currentRoom = "FrontDoor"
 def addToList(item):
         
     try:
-        if playerAction[1] == "all":
+        if playerTarget == "all":
             itemList.extend(roomItems[currentRoom])
             roomItems[currentRoom].clear()
         else:
-            itemList.  append(roomItems[currentRoom].  pop(  roomItems.get(currentRoom).  index(playerAction[1]  )))
+            itemList.  append(roomItems[currentRoom].  pop(  roomItems.get(currentRoom).  index(playerTarget)))
     except:
-        print (playerAction[1] + " cannot be taken.")
+        print (playerTarget + " cannot be taken.")
 
 def removeFromList(item):
     try:
-        if playerAction[1] == "all":
+        if playerTarget == "all":
             roomItems[currentRoom].extend (itemList)
             itemList.clear()
         else:
-            roomItems[currentRoom].  append(itemList.  pop(  itemList.  index(playerAction[1]  )))
+            roomItems[currentRoom].  append(itemList.  pop(  itemList.  index(playerTarget  )))
     except:
-        print ("You do not have a " + playerAction[1] + ".")
+        print ("You do not have a " + playerTarget + ".")
         
 def printInventory():
     itemList.sort()
@@ -174,40 +243,57 @@ def go(direction):
 
 
 
-while(True):
+while True:
     try:
-        playerInput = input()
+        playerInput = input()+" "
         playerInput.lower ()
-                      
-        playerAction = playerInput.split(" ")
+        playerInputSplit = playerInput.split(" ")
+     
+        playerAction = "null"
+        playerTarget = "null"
         
-        if playerAction[0] == "take":
-            addToList(playerAction[1])
-            
-        if playerAction[0] == "drop":
-            removeFromList(playerAction[1])
+        if playerInputSplit[0] in actionDictionary:
+            playerAction = actionDictionary[playerInputSplit[0]]
+        elif playerInputSplit[0] in directionDictionary:
+            playerAction = directionDictionary[playerInputSplit[0]]
+    
+        if playerInputSplit[1] in itemDictionary:
+            playerTarget = itemDictionary[playerInputSplit[1]]
+        elif playerInputSplit[1] in directionDictionary:
+            playerTarget = directionDictionary[playerInputSplit[1]]
 
-        if playerAction[0] == "i":
+
+            
+
+        
+        if playerAction == "take":
+            addToList(playerAction)
+            
+        if playerAction == "drop":
+            removeFromList(playerAction)
+
+        if playerAction == "use":
+            ## room specific item usage checks
+
+        if playerAction == "inventory":
             printInventory()
 
-        if playerAction[0] == "go":
-            currentRoom = go(playerAction[1])
-        if playerAction[0] == "north":
-            currentRoom = go(playerAction[0])
-        if playerAction[0] == "east":
-            currentRoom = go(playerAction[0])
-        if playerAction[0] == "south":
-            currentRoom = go(playerAction[0])
-        if playerAction[0] == "west":
-            currentRoom = go(playerAction[0])
-        if playerAction[0] == "up":
-            currentRoom = go(playerAction[0])
-        if playerAction[0] == "down":
-            currentRoom = go(playerAction[0])
-        
-        
-        if playerAction[0] == "l":
+        if playerAction == "go" and playerTarget in directionDictionary:
+            currentRoom = go(playerTarget)
+        if playerAction in directionDictionary:
+            currentRoom = go(playerAction)
+
+        if playerAction[0] == "look":
             look()
+            
+        if playerAction[0] == "save":
+            ## save roomStatus (dictionary), roomItems (dictionary), itemList (list), currentRoom (string)
+
+        if playerAction[0] == "load":            
+            ## load roomStatus (dictionary), roomItems (dictionary), itemList (list), currentRoom (string)
+            
+        if playerAction[0] == "quit":            
+            ## end the game
 
     except:
         print ("That was not a recognised action.")
