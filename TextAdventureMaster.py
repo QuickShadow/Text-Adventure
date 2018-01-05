b095c29efcd55359bed2fa05e06348cb1d542271
QuickShadow <chaosmagus@hotmail.co.uk>
2018-01-05 23:58:56 +0000

    General Update
    Added rooms to top floor, placed remaining items.
    Still need room descriptions and final area text.  Need full playthrough test.

--- TextAdventureMaster.py
+++ TextAdventureMaster.py
@@ -84,7 +84,7 @@ itemDescription ["diary"] =     "A handwritten diary kept by one of the servants
 itemDescription ["ladder"]=     "An old wooden ladder."
 itemDescription ["lamp"] =      "A brass bedside lamp with a wick and paraffin burner."
 itemDescription ["letter"] =    "The letter from your dear friend Marcus Callaghan which brought you here."
-itemDescription ["key"] =       "A heavy brass key."
+itemDescription ["key"] =       "A heavy brass keyring with a couple of keys on it."
 itemDescription ["telescope"] = "An ornate telescope that seems to have been modified with extra lenses.\nA power cable runs from it into the floor."
 itemDescription ["match"] =     "A cardboard matchbox full of long matches with a striker on the outside of the box."
 itemDescription ["starch"] =    "A damp box of starch flakes used for laundry."
@@ -92,6 +92,15 @@ itemDescription ["starch"] =    "A damp box of starch flakes used for laundry."
 ##These variables set the starting condition of the game
 itemList = ["letter"]
 currentRoom = "FrontDoor"
+
+##Import string for ASCii character strings
+import string
+##Import random for random number generator
+import random
+##Create a 6 digit random string
+planet = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+print (planet)
 
 
 ## These definitions link actions to key word subroutines
@@ -196,6 +205,10 @@ def useItem(item):
             if currentRoom == "Study":
                 print ("You open the drawer.\nThere is a worn leather journal here.")
                 roomItems[currentRoom].append("journal")
+            ## Adds the Diary to available items the first time the servants quarters 2 drawer is opened
+            elif currentRoom == "ServantQuarters2":
+                print ("You open the drawer.\nYou find a small book inside, it looks like a diary.")
+                roomItems[currentRoom].append("diary")
             else:
                 print ("You open the drawer.\nThe drawer is empty.")
             roomInteractive[currentRoom]["drawer"]="open"
@@ -244,14 +257,41 @@ def useItem(item):
             itemList.remove("starch")
             print ("Using the last of the starch flakes you quickly complete the circle design laid out on the floor.\nThe symbol is now complete again.")
 
-    else:
+    elif item == "lamp" and currentRoom == "DarkHallway":
+        if "lit match" in itemList:
+            print ("Using the match to light the taper produces a soft yellowish glow.\nAlmost immediately a strong gust of wind comes from nowhere as if trying to blow out the light\nbut you quickly close the glass door on the lamp, protecting the flame.\nA handy nail on the wall provides a good place to hang the lamp by the handle, giving good light through the hallway.\nYou swear you could hear something hiss in the shadows and the sound of scuttering feet moving away from you.")
+            itemList.remove("lamp")
+            roomStatus["DarkHallway"] = 1
+        else:
+            print ("You have nothing to light the lamp with.")
+
+    elif item == "key" and currentRoom == "ServantsStairs":
+        print ("One of the keys fits the lock on the door, and it swings gently open revealing the service area at the back of the dining hall.")
+        itemList.remove("key")
+        roomStatus["ServiceArea"] = 1
+        roomStatus["ServantsStairs"] = 1 
+
+    elif item == "diary":
+        print ("Flicking through the diary a couple of entries catch your eye.  The first is dated about a month ago:\n'  I was curious about the Masters experiments so I followed him on my afternoon off.\nI still have no clue what he was doing, but he's been spending a lot of time in the old observatory,\nand when I followed him into the library this evening he walked to the fire,\nand then vanished before I got around the corner.\nThere's definitely something strange going on...'\n\nThe second entry is only a few days old,\n'  I heard it again last night.  I don't care what they say that's no rat down there, it sounds huge.  I blocked the door just to be safe, but it seems to like the darkness down there...'")
+        roomStatus["Library"] = 1
+
+    elif item == "telescope":
+        print ("You bend to look through the scope, but the view doesn't seem to be focused on anything.\nWhat would you like to look at?")
+        viewTarget = input()
+        viewTarget.upper
+        if viewTarget == "MARS" or viewTarget == "JUPITER" or viewTarget == "SATURN" :
+            print ("It takes a moment for you to find the planet, and slightly longer to understand what you are looking at.\nThe planets all seem to be lined up, eclipsing each other.")
+        elif viewTarget == planet:
+            print ("The small planet gleams brightly in the sky.  You wonder if it is getting brighter?\nA ray of light hits the telescope, lighting up the lenses before being focused into a device at the top of the cable.\nSoon after you hear the soft hum of an electric generator below you.")
+            
+        else:
+            print (viewTarget + " is not visible from this telescope tonight.")
+            roomInteractive[currentRoom]["telescope"] = "found"
+
+    else:            
         print ("You cannot use that item here.")
 
 ##itemDictionary ["journal"] = "journal"
-##itemDictionary ["diary"] = "diary"
-##itemDictionary ["lamp"] = "lamp"
-##itemDictionary ["key"] = "key"
-##itemDictionary ["telescope"] = "telescope"
 
 
 
@@ -321,5 +361,8 @@ while True:
 
 ##Passive room status updates
     if currentRoom == "Pantry" and roomStatus["Pantry"] == 0:
-        roomStatus["Pantry"]=1
-        roomStatus["ServantsHall2"]=1
+        roomStatus["Pantry"] = 1
+        roomStatus["ServantsHall2"] = 1
+
+    if  roomStatus["Solarium"] == 0 and roomInteractive["Observatory"]["telescope"] == "found" and roomStatus["WineCellar"] == 1:
+        roomStatus["Solarium"] = 1
--- TextAdventureRooms.py
+++ TextAdventureRooms.py
@@ -70,18 +70,18 @@ roomItems       ["Kitchen"] = ["match"]
 roomInteractive ["Kitchen"] = {"drawer":"closed"}
 
 roomStatus      ["Pantry"] = 0
-roomDescription ["Pantry"] = ["This is a description of the Pantry when the player falls in from above.", "This is a description of the pantry."]
+roomDescription ["Pantry"] = ["The creaking floor finally gives way as you walk across it, taking the ground from beneath your feet.\nYou fall into the darkness below and it takes a moment for your head to stop spinning.\nLooking around you seem to have landed in the storage pantry for the main kitchen.\nFeeling slightly disorientated you hope you can find your way back upstairs.\nA door to the north seems to be the only way out.", "The large hole in the celing of the pantry serves as a reminder of your fall from the corridor above.\nThere seems to be a lot of food goods stored here, and the only door out on the north side leads out into the kitchen.\nThe door is open despite having a rather solid looking lock on it."]
 roomNorth       ["Pantry"] = ["null", "Kitchen"]
 roomEast        ["Pantry"] = ["null", "null"]
 roomSouth       ["Pantry"] = ["null", "null"]
 roomWest        ["Pantry"] = ["null", "null"]
 roomUp          ["Pantry"] = ["null", "null"]
 roomDown        ["Pantry"] = ["null", "null"]
-roomItems       ["Pantry"] = []
+roomItems       ["Pantry"] = ["key"]
 roomInteractive ["Pantry"] = {}
 
 roomStatus      ["DarkHallway"] = 0
-roomDescription ["DarkHallway"] = ["This is a description of the Dark Hallway.", "This is a description of the Dark Hallway after the lamp has been used."]
+roomDescription ["DarkHallway"] = ["The hallway is too dark to see where you are going.\nAfter a moment of trying to let your eyes adjust,you realise the only thing you can see is the kitchen doorway at the north end of the hall.", "By the lamplight you can see several doors leading from the hall.\nThe kitchen is at the left to the north,\na door to the east seems to lead outside,\nanother dim room to the west of the hall,\nand some stairs leading up at the south."]
 roomNorth       ["DarkHallway"] = ["Kitchen", "Kitchen"]
 roomEast        ["DarkHallway"] = ["null",    "HoHo2"]
 roomSouth       ["DarkHallway"] = ["null",    "ShadowStairs"]
@@ -116,23 +116,23 @@ roomItems       ["SecretStairs"] = []
 roomInteractive ["SecretStairs"] = {}
 
 roomStatus      ["ServantsStairs"] = 0
-roomDescription ["ServantsStairs"] = ["This is a description of the Servants Stairs."]
-roomNorth       ["ServantsStairs"] = ["null"]
-roomEast        ["ServantsStairs"] = ["null"]
-roomSouth       ["ServantsStairs"] = ["null"]
-roomWest        ["ServantsStairs"] = ["null"]
-roomUp          ["ServantsStairs"] = ["ServiceArea"]
-roomDown        ["ServantsStairs"] = ["Kitchen"]
+roomDescription ["ServantsStairs"] = ["The servants stairs wind upwards through the back of the house coming to a small landing.\nA heavy door stands at the top, firmly dividing the staff area and the family area with a solid lock.\nThe only other way is back down to the kitchen.","The servants stairs wind through the back wall of the building with the service area up at the top\nand the main galley kitchen down at the bottom."]
+roomNorth       ["ServantsStairs"] = ["null",       "null"]
+roomEast        ["ServantsStairs"] = ["null",       "null"]
+roomSouth       ["ServantsStairs"] = ["null",       "null"]
+roomWest        ["ServantsStairs"] = ["null",       "null"]
+roomUp          ["ServantsStairs"] = ["null",       "ServiceArea"]
+roomDown        ["ServantsStairs"] = ["Kitchen",    "Kitchen"]
 roomItems       ["ServantsStairs"] = []
 roomInteractive ["ServantsStairs"] = {}
 
 roomStatus      ["ShadowStairs"] = 0
-roomDescription ["ShadowStairs"] = ["This is a description of the Shadow Stairs."]
+roomDescription ["ShadowStairs"] = ["These stairs lead upwards, but come to an abrupt end at a door.\nIt seems to have been barricaded from the other side.\nThere is nowhere to go but back down to the hall."]
 roomNorth       ["ShadowStairs"] = ["null"]
 roomEast        ["ShadowStairs"] = ["null"]
 roomSouth       ["ShadowStairs"] = ["null"]
 roomWest        ["ShadowStairs"] = ["null"]
-roomUp          ["ShadowStairs"] = ["ServantsHall3"]
+roomUp          ["ShadowStairs"] = ["null"]
 roomDown        ["ShadowStairs"] = ["DarkHallway"]
 roomItems       ["ShadowStairs"] = []
 roomInteractive ["ShadowStairs"] = {}
@@ -158,8 +158,8 @@ roomSouth       ["HeadOfHouseQuarters"] = ["null"]
 roomWest        ["HeadOfHouseQuarters"] = ["ServantsHall1"]
 roomUp          ["HeadOfHouseQuarters"] = ["null"]
 roomDown        ["HeadOfHouseQuarters"] = ["null"]
-roomItems       ["HeadOfHouseQuarters"] = []
-roomInteractive ["HeadOfHouseQuarters"] = {}
+roomItems       ["HeadOfHouseQuarters"] = ["candle"]
+roomInteractive ["HeadOfHouseQuarters"] = {"drawer":"closed"}
 
 roomStatus      ["ServantQuarters1"] = 0
 roomDescription ["ServantQuarters1"] = ["This is a description of the Servants Quarters 1."]
@@ -170,7 +170,7 @@ roomWest        ["ServantQuarters1"] = ["ServantsHall2"]
 roomUp          ["ServantQuarters1"] = ["null"]
 roomDown        ["ServantQuarters1"] = ["null"]
 roomItems       ["ServantQuarters1"] = []
-roomInteractive ["ServantQuarters1"] = {}
+roomInteractive ["ServantQuarters1"] = {"drawer":"closed"}
 
 roomStatus      ["ServantQuarters2"] = 0
 roomDescription ["ServantQuarters2"] = ["This is a description of the Servants Quarters 2."]
@@ -181,7 +181,7 @@ roomWest        ["ServantQuarters2"] = ["ServantsHall3"]
 roomUp          ["ServantQuarters2"] = ["null"]
 roomDown        ["ServantQuarters2"] = ["null"]
 roomItems       ["ServantQuarters2"] = []
-roomInteractive ["ServantQuarters2"] = {}
+roomInteractive ["ServantQuarters2"] = {"drawer":"default"}
 
 roomStatus      ["FrontDoor"] = 0
 roomDescription ["FrontDoor"] = ["This is a description of the Front Door."]
@@ -224,17 +224,17 @@ roomSouth       ["DiningHall"] = ["MainHall"]
 roomWest        ["DiningHall"] = ["null"]
 roomUp          ["DiningHall"] = ["null"]
 roomDown        ["DiningHall"] = ["null"]
-roomItems       ["DiningHall"] = []
+roomItems       ["DiningHall"] = ["candle"]
 roomInteractive ["DiningHall"] = {}
 
 roomStatus      ["ServiceArea"] = 0
-roomDescription ["ServiceArea"] = ["This is a description of the Service Area."]
-roomNorth       ["ServiceArea"] = ["null"]
-roomEast        ["ServiceArea"] = ["ServantsHall1"]
-roomSouth       ["ServiceArea"] = ["null"]
-roomWest        ["ServiceArea"] = ["DiningHall"]
-roomUp          ["ServiceArea"] = ["null"]
-roomDown        ["ServiceArea"] = ["ServantsStairs"]
+roomDescription ["ServiceArea"] = ["This is a description of the Service Area.", "This is a description of the Service Area with the door unlocked."]
+roomNorth       ["ServiceArea"] = ["null",          "null"]
+roomEast        ["ServiceArea"] = ["ServantsHall",  "ServantsHall1"]
+roomSouth       ["ServiceArea"] = ["null",          "null"]
+roomWest        ["ServiceArea"] = ["DiningHall",    "DiningHall"]
+roomUp          ["ServiceArea"] = ["null",          "null"]
+roomDown        ["ServiceArea"] = ["null",          "ServantsStairs"]
 roomItems       ["ServiceArea"] = []
 roomInteractive ["ServiceArea"] = {}
 
@@ -307,25 +307,114 @@ roomInteractive ["HiddenRoom"] = {}
 
 ## First Floor
 
-##The following rooms still need to added:
-##Bedroom 1
-##Bathroom 1
-##Observatory
-##Governess Quarters
-##Nursery
-##Cupboard 1
-##Bathroom 2
-##Master Bedroom
-##Cupboard 2
-##Bedroom 2
-
 roomStatus      ["CentralLanding"] = 0
 roomDescription ["CentralLanding"] = ["This is a description of the Central Landing."]
-roomNorth       ["CentralLanding"] = ["null"]
-roomEast        ["CentralLanding"] = ["null"]
+roomNorth       ["CentralLanding"] = ["Observatory"]
+roomEast        ["CentralLanding"] = ["EastLanding"]
 roomSouth       ["CentralLanding"] = ["null"]
 roomWest        ["CentralLanding"] = ["null"]
 roomUp          ["CentralLanding"] = ["null"]
 roomDown        ["CentralLanding"] = ["MainHall"]
 roomItems       ["CentralLanding"] = []
 roomInteractive ["CentralLanding"] = {}
+
+roomStatus      ["EastLanding"] = 0
+roomDescription ["EastLanding"] = ["This is a description of the East Landing."]
+roomNorth       ["EastLanding"] = ["null"]
+roomEast        ["EastLanding"] = ["Nursery"]
+roomSouth       ["EastLanding"] = ["GovernessQuarters"]
+roomWest        ["EastLanding"] = ["CentralLanding"]
+roomUp          ["EastLanding"] = ["null"]
+roomDown        ["EastLanding"] = ["null"]
+roomItems       ["EastLanding"] = []
+roomInteractive ["EastLanding"] = {}
+
+roomStatus      ["WestLanding"] = 0
+roomDescription ["WestLanding"] = ["This is a description of the West Landing."]
+roomNorth       ["WestLanding"] = ["Bedroom1"]
+roomEast        ["WestLanding"] = ["CentralLanding"]
+roomSouth       ["WestLanding"] = ["MasterBedroom"]
+roomWest        ["WestLanding"] = ["Bedroom2"]
+roomUp          ["WestLanding"] = ["null"]
+roomDown        ["WestLanding"] = ["null"]
+roomItems       ["WestLanding"] = []
+roomInteractive ["WestLanding"] = {}
+
+roomStatus      ["Observatory"] = 0
+roomDescription ["Observatory"] = ["This is a description of the Observatory."]
+roomNorth       ["Observatory"] = ["null"]
+roomEast        ["Observatory"] = ["null"]
+roomSouth       ["Observatory"] = ["CentralLanding"]
+roomWest        ["Observatory"] = ["null"]
+roomUp          ["Observatory"] = ["null"]
+roomDown        ["Observatory"] = ["null"]
+roomItems       ["Observatory"] = []
+roomInteractive ["Observatory"] = {"telescope":"looking"}
+
+roomStatus      ["Bedroom1"] = 0
+roomDescription ["Bedroom1"] = ["This is a description of Bedroom 1."]
+roomNorth       ["Bedroom1"] = ["null"]
+roomEast        ["Bedroom1"] = ["null"]
+roomSouth       ["Bedroom1"] = ["WestLanding"]
+roomWest        ["Bedroom1"] = ["null"]
+roomUp          ["Bedroom1"] = ["null"]
+roomDown        ["Bedroom1"] = ["null"]
+roomItems       ["Bedroom1"] = ["candle"]
+roomInteractive ["Bedroom1"] = {"drawer":"closed"}
+
+roomStatus      ["Bedroom2"] = 0
+roomDescription ["Bedroom2"] = ["This is a description of Bedroom 2."]
+roomNorth       ["Bedroom2"] = ["null"]
+roomEast        ["Bedroom2"] = ["WestLanding"]
+roomSouth       ["Bedroom2"] = ["null"]
+roomWest        ["Bedroom2"] = ["null"]
+roomUp          ["Bedroom2"] = ["null"]
+roomDown        ["Bedroom2"] = ["null"]
+roomItems       ["Bedroom2"] = ["candle"]
+roomInteractive ["Bedroom2"] = {"drawer":"closed"}
+
+roomStatus      ["MasterBedroom"] = 0
+roomDescription ["MasterBedroom"] = ["This is a description of the Master Bedroom."]
+roomNorth       ["MasterBedroom"] = ["WestLanding"]
+roomEast        ["MasterBedroom"] = ["null"]
+roomSouth       ["MasterBedroom"] = ["null"]
+roomWest        ["MasterBedroom"] = ["null"]
+roomUp          ["MasterBedroom"] = ["null"]
+roomDown        ["MasterBedroom"] = ["null"]
+roomItems       ["MasterBedroom"] = ["lamp"]
+roomInteractive ["MasterBedroom"] = {"drawer":"closed"}
+
+roomStatus      ["Nursery"] = 0
+roomDescription ["Nursery"] = ["This is a description of the Nursery."]
+roomNorth       ["Nursery"] = ["null"]
+roomEast        ["Nursery"] = ["null"]
+roomSouth       ["Nursery"] = ["GovernessQuarters"]
+roomWest        ["Nursery"] = ["EastLanding"]
+roomUp          ["Nursery"] = ["null"]
+roomDown        ["Nursery"] = ["null"]
+roomItems       ["Nursery"] = []
+roomInteractive ["Nursery"] = {}
+roomStatus      ["Nursery"] = 0
+
+roomDescription ["GovernessQuarters"] = ["This is a description of the Governess Quarters."]
+roomNorth       ["GovernessQuarters"] = ["Nursery"]
+roomEast        ["GovernessQuarters"] = ["null"]
+roomSouth       ["GovernessQuarters"] = ["null"]
+roomWest        ["GovernessQuarters"] = ["EastLanding"]
+roomUp          ["GovernessQuarters"] = ["null"]
+roomDown        ["GovernessQuarters"] = ["null"]
+roomItems       ["GovernessQuarters"] = ["candle"]
+roomInteractive ["GovernessQuarters"] = {"drawer":"closed"}
+
+## Special Room
+
+roomStatus      ["???"] = 0
+roomDescription ["???"] = ["This is a description of ???."]
+roomNorth       ["???"] = ["null"]
+roomEast        ["???"] = ["null"]
+roomSouth       ["???"] = ["null"]
+roomWest        ["???"] = ["null"]
+roomUp          ["???"] = ["null"]
+roomDown        ["???"] = ["null"]
+roomItems       ["???"] = []
+roomInteractive ["???"] = {}
